a3f3abd159e382209d60f1f7fb825ca5478fbeae
youfeng <youfeng@haizhi.com>
2017-08-17 22:45:44 +0800

    修改mongo导出程序

--- export_mongo.sh
+++ export_mongo.sh
@@ -4,4 +4,4 @@ dump_path=~
 
 ./mongodump -h 172.16.215.16:40042 -d app_data -c register_code_clean  -u read -p read -o ${dump_path}
 
-zip app_data.zip -r app_data/
+zip app_data.zip -r ${dump_path}/app_data/
